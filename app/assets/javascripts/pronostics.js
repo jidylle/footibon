4751f2587dd58cb3c479281be1b31a0fee9c9c40
Jerome IDYLLE <gg@MacBook-Pro-de-Jerome-IDYLLE.local>
2014-06-19 16:31:52 -0400

    fix issue share js

--- app/assets/javascripts/pronostics.js
+++ app/assets/javascripts/pronostics.js
@@ -1,10 +1,14 @@
-function fbShare(url, title, descr, image, winWidth, winHeight) {
-var winTop = (screen.height / 2) - (winHeight / 2);
-var winLeft = (screen.width / 2) - (winWidth / 2);
-window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
-}
-
-$('.datatable').dataTable({
-    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-    "sPaginationType": "bootstrap"
+$('#share_button').click(function(e){
+    e.preventDefault();
+    FB.ui(
+        {
+            method: 'feed',
+            name: user_name +' pense que le match '+ match_name+' se terminera sur le score de '+prono_score,
+            link: url,
+            picture: image_url,
+            caption: 'Toi aussi, défis tes amis et donne ton pronostic sur FootiBon',
+            description: 'FootiBon est un jeu fun et gratuit permettant de défier ses amis en pronostiquant les résultats des matchs du mondiale 2014. Rdv sur http://www.footibon.com',
+            message : prono_score+', voici mon pronostic pour le match '+ match_name + ". Qu'en penses-tu?"
+        });
 });
+
