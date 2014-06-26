a41482b01ee7e8daf4a44d081cb1c69e1e84f67a
Jerome IDYLLE <gg@MacBook-Pro-de-Jerome-IDYLLE.local>
2014-06-25 20:13:48 -0400

    add share button

--- app/assets/javascripts/pronostics.js
+++ app/assets/javascripts/pronostics.js
@@ -12,21 +12,22 @@ $(document).ready(function(){
             }
         };
     })();
+
+    $('#share_button').click(function(e){
+        e.preventDefault();
+        FB.ui(
+            {
+                method: 'feed',
+                name: user_name +' pense que le match '+ match_name+' se terminera sur le score de '+prono_score,
+                link: url,
+                picture: image_url,
+                description: 'Faites le match dans le match sur FootiBon.com Le meilleur endroit pour défier ses amis en pronostiquant les résultats des matchs du mondial 2014. Rdv sur http://www.footibon.com',
+                message : prono_score+', voici mon pronostic pour le match '+ match_name + ". Qu'en penses-tu?"
+            });
+    });
 });
 
-$('#share_button').click(function(e){
-    e.preventDefault();
-    FB.ui(
-        {
-            method: 'feed',
-            name: user_name +' pense que le match '+ match_name+' se terminera sur le score de '+prono_score,
-            link: url,
-            picture: image_url,
-            caption: 'Toi aussi, défend ton pronostic et défis tes amis sur FootiBon',
-            description: 'FootiBon est un jeu fun et gratuit permettant de s\'amuser entre amis en pronostiquant les résultats des matchs du mondial 2014. Rdv sur http://www.footibon.com',
-            message : prono_score+', voici mon pronostic pour le match '+ match_name + ". Qu'en penses-tu?"
-        });
-});
+
 
 
 
